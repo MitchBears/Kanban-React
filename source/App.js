ed1fa6d8f3e64ef135d8360d8b5a324577e5949a
MitchBears <mitchell.bears@mail.mcgill.ca>
2017-05-01 13:35:09 -0400

    Up and running. Added propTypes and css styling

--- source/App.js
+++ source/App.js
@@ -1,36 +1,28 @@
 import React from 'react';
+import {render} from 'react-dom';
 import KanbanBoard from './KanbanBoard';
 
-let cardList = [
-    {
-        id: 1,
-        title: "Read the book",
-        description: "I should read the entire book",
-        status: "in-progress",
-        tasks: []
-    },
-    {
-        id: 2,
-        title: "Write some code",
-        description: "Code along witht the samples in the book",
-        status: "todo",
-        tasks: [
-            {
-            id: 1,
-            name: "ContactList Example",
-            },
-            {
-            id: 2,
-            name: "Kanban Example",
-            done: false
-            },
-            {
-            id: 3,
-            name: "My own experiments",
-            done: false
-            }
-        ]
-    }
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the **whole** book",
+    color: '#BD8D31',
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
+    color: '#3A7E28',
+    status: "todo",
+    tasks: [
+      {id: 1, name: "ContactList Example", done: true},
+      {id: 2, name: "Kanban Example", done: false},
+      {id: 3, name: "My own experiments", done:false}
+    ]
+  }
 ];
 
-React.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
+render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
---
+++ source/Card.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import CheckList from './CheckList';
+import marked from 'marked';
+
+let titlePropType = (props, propName, componentName) => {
+  if (props[propName]){
+    let value = props[propName];
+    if (typeof value !== 'string' || value.length > 80){
+      return new Error(
+         `${propName} in ${componentName} is longer than 80 characters` 
+      )
+    }
+  }
+}
+
+class Card extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showDetails: false
+    }
+  };
+
+  toggleDetails(){
+    this.setState({showDetails: !this.state.showDetails});
+  }
+  
+  render() {
+    let cardDetails;
+    if (this.state.showDetails) {
+      cardDetails = (
+        <div className="card__details">
+          <span dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
+          <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+        </div>
+      );
+    };
+    let sidecolor = {
+      position: 'absolute',
+      zIndex: -1,
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: 7, 
+      backgroundColor: this.props.color
+    };
+    return (
+      <div className="card">
+        <div style={sidecolor}/>
+        <div className={
+          this.state.showDetails ? "card_title card_title --is-open": "card_title"}
+           onClick={this.toggleDetails.bind(this)}>{this.props.title}</div>
+        {cardDetails}
+      </div>
+    );
+  }
+};
+Card.propTypes = {
+  id: PropTypes.number,
+  title: titlePropType, //how does this assign the correct arguments to titlePropType. You never specify anything. 
+  description: PropTypes.string,
+  color: PropTypes.string,
+  tasks: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default Card;
